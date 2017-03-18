528758feed8fc530e5557cd0ed437a27afb34da7
jntakpe <jntakpe@gmail.com>
2017-03-18 14:52:22 +0100

    Ajout de comptes utilisateurs pour les devs dans auth-service

--- auth-service/src/main/resources/import.sql
+++ auth-service/src/main/resources/import.sql
@@ -3,10 +3,18 @@
 -- Table user
 INSERT INTO user (username, password) VALUES ('jntakpe', 'test');
 INSERT INTO user (username, password) VALUES ('cbarillet', 'test');
-INSERT INTO user (username, password) VALUES ('sberger', 'test');
-INSERT INTO user (username, password) VALUES ('smaitre', 'test');
-INSERT INTO user (username, password) VALUES ('ccavelier', 'test');
-INSERT INTO user (username, password) VALUES ('tmarchandise', 'test');
+INSERT INTO user (username, password) VALUES ('rjansem', 'test');
+INSERT INTO user (username, password) VALUES ('cegiraud', 'test');
+INSERT INTO user (username, password) VALUES ('crinfray', 'test');
+INSERT INTO user (username, password) VALUES ('anycz', 'test');
+INSERT INTO user (username, password) VALUES ('fleriche', 'test');
+INSERT INTO user (username, password) VALUES ('mbouhamyd', 'test');
+INSERT INTO user (username, password) VALUES ('nmpacko', 'test');
+INSERT INTO user (username, password) VALUES ('ocoulibaly', 'test');
+INSERT INTO user (username, password) VALUES ('mmarquez', 'test');
+INSERT INTO user (username, password) VALUES ('tchoteau', 'test');
+INSERT INTO user (username, password) VALUES ('eparisot', 'test');
+
 
 -- Table authority
 INSERT INTO authority (name) VALUES ('USER');
@@ -17,10 +25,27 @@ INSERT INTO user_authorities(user_id, authority_id) VALUES ((SELECT id FROM user
 INSERT INTO user_authorities(user_id, authority_id) VALUES ((SELECT id FROM user WHERE username='jntakpe'), (SELECT id FROM authority WHERE name='ADMIN'));
 INSERT INTO user_authorities(user_id, authority_id) VALUES ((SELECT id FROM user WHERE username='cbarillet'), (SELECT id FROM authority WHERE name='USER'));
 INSERT INTO user_authorities(user_id, authority_id) VALUES ((SELECT id FROM user WHERE username='cbarillet'), (SELECT id FROM authority WHERE name='ADMIN'));
-INSERT INTO user_authorities(user_id, authority_id) VALUES ((SELECT id FROM user WHERE username='sberger'), (SELECT id FROM authority WHERE name='USER'));
-INSERT INTO user_authorities(user_id, authority_id) VALUES ((SELECT id FROM user WHERE username='smaitre'), (SELECT id FROM authority WHERE name='USER'));
-INSERT INTO user_authorities(user_id, authority_id) VALUES ((SELECT id FROM user WHERE username='smaitre'), (SELECT id FROM authority WHERE name='ADMIN'));
-INSERT INTO user_authorities(user_id, authority_id) VALUES ((SELECT id FROM user WHERE username='ccavelier'), (SELECT id FROM authority WHERE name='USER'));
-INSERT INTO user_authorities(user_id, authority_id) VALUES ((SELECT id FROM user WHERE username='ccavelier'), (SELECT id FROM authority WHERE name='ADMIN'));
+INSERT INTO user_authorities(user_id, authority_id) VALUES ((SELECT id FROM user WHERE username='rjansem'), (SELECT id FROM authority WHERE name='USER'));
+INSERT INTO user_authorities(user_id, authority_id) VALUES ((SELECT id FROM user WHERE username='rjansem'), (SELECT id FROM authority WHERE name='ADMIN'));
+INSERT INTO user_authorities(user_id, authority_id) VALUES ((SELECT id FROM user WHERE username='cegiraud'), (SELECT id FROM authority WHERE name='USER'));
+INSERT INTO user_authorities(user_id, authority_id) VALUES ((SELECT id FROM user WHERE username='cegiraud'), (SELECT id FROM authority WHERE name='ADMIN'));
+INSERT INTO user_authorities(user_id, authority_id) VALUES ((SELECT id FROM user WHERE username='crinfray'), (SELECT id FROM authority WHERE name='USER'));
+INSERT INTO user_authorities(user_id, authority_id) VALUES ((SELECT id FROM user WHERE username='crinfray'), (SELECT id FROM authority WHERE name='ADMIN'));
+INSERT INTO user_authorities(user_id, authority_id) VALUES ((SELECT id FROM user WHERE username='anycz'), (SELECT id FROM authority WHERE name='USER'));
+INSERT INTO user_authorities(user_id, authority_id) VALUES ((SELECT id FROM user WHERE username='anycz'), (SELECT id FROM authority WHERE name='ADMIN'));
+INSERT INTO user_authorities(user_id, authority_id) VALUES ((SELECT id FROM user WHERE username='fleriche'), (SELECT id FROM authority WHERE name='USER'));
+INSERT INTO user_authorities(user_id, authority_id) VALUES ((SELECT id FROM user WHERE username='fleriche'), (SELECT id FROM authority WHERE name='ADMIN'));
+INSERT INTO user_authorities(user_id, authority_id) VALUES ((SELECT id FROM user WHERE username='mbouhamyd'), (SELECT id FROM authority WHERE name='USER'));
+INSERT INTO user_authorities(user_id, authority_id) VALUES ((SELECT id FROM user WHERE username='mbouhamyd'), (SELECT id FROM authority WHERE name='ADMIN'));
+INSERT INTO user_authorities(user_id, authority_id) VALUES ((SELECT id FROM user WHERE username='nmpacko'), (SELECT id FROM authority WHERE name='USER'));
+INSERT INTO user_authorities(user_id, authority_id) VALUES ((SELECT id FROM user WHERE username='nmpacko'), (SELECT id FROM authority WHERE name='ADMIN'));
+INSERT INTO user_authorities(user_id, authority_id) VALUES ((SELECT id FROM user WHERE username='ocoulibaly'), (SELECT id FROM authority WHERE name='USER'));
+INSERT INTO user_authorities(user_id, authority_id) VALUES ((SELECT id FROM user WHERE username='ocoulibaly'), (SELECT id FROM authority WHERE name='ADMIN'));
+INSERT INTO user_authorities(user_id, authority_id) VALUES ((SELECT id FROM user WHERE username='mmarquez'), (SELECT id FROM authority WHERE name='USER'));
+INSERT INTO user_authorities(user_id, authority_id) VALUES ((SELECT id FROM user WHERE username='mmarquez'), (SELECT id FROM authority WHERE name='ADMIN'));
+INSERT INTO user_authorities(user_id, authority_id) VALUES ((SELECT id FROM user WHERE username='tchoteau'), (SELECT id FROM authority WHERE name='ADMIN'));
+INSERT INTO user_authorities(user_id, authority_id) VALUES ((SELECT id FROM user WHERE username='tchoteau'), (SELECT id FROM authority WHERE name='ADMIN'));
+INSERT INTO user_authorities(user_id, authority_id) VALUES ((SELECT id FROM user WHERE username='eparisot'), (SELECT id FROM authority WHERE name='ADMIN'));
+INSERT INTO user_authorities(user_id, authority_id) VALUES ((SELECT id FROM user WHERE username='eparisot'), (SELECT id FROM authority WHERE name='ADMIN'));
 
 --@formatter:on
