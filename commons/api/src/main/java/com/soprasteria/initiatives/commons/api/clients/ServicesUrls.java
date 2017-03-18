f7b1686fb9d57a4692af39ea146d3c231b236e06
jntakpe <jntakpe@gmail.com>
2017-03-18 14:41:36 +0100

    Suppression des anciennes urls vers les services

--- commons/api/src/main/java/com/soprasteria/initiatives/commons/api/clients/ServicesUrls.java
+++ commons/api/src/main/java/com/soprasteria/initiatives/commons/api/clients/ServicesUrls.java
@@ -7,19 +7,4 @@ package com.soprasteria.initiatives.commons.api.clients;
  */
 final class ServicesUrls {
 
-    static final String ACCOUNT_VIEWING_SERVICE = "account-viewing-service";
-
-    static final String ACCOUNT_VIEWING_SERVICE_TEST_URL = "${account-viewing.test.url:}";
-
-    //TODO cba : think about naming
-    static final String CONNECTOR_CORE_BANKING_A = "connector-core-banking-a";
-
-    //TODO cba : think about naming
-    static final String CONNECTOR_CORE_BANKING_A_TEST_URL = "${connector-core-banking-a.test.url:http://localhost:9080/connector-core-banking-a}";
-
-    //TODO cba : think about naming
-    static final String CORE_BANKING_A = "core-banking-a";
-
-    //TODO cba : think about naming
-    static final String CORE_BANKING_A_TEST_URL = "${core-banking-a.test.url:http://localhost:9080/core-banking-a}";
 }
