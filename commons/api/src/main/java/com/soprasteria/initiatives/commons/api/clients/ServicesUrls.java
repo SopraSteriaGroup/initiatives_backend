a7961a6256fbf90e668a84518415f6ae23810f9f
Charles-Éric Giraud <charles-eric.giraud@soprasteria.com>
2017-03-19 10:53:47 +0100

    ajout des constantes manquantes pour pouvoir compiler

--- commons/api/src/main/java/com/soprasteria/initiatives/commons/api/clients/ServicesUrls.java
+++ commons/api/src/main/java/com/soprasteria/initiatives/commons/api/clients/ServicesUrls.java
@@ -7,4 +7,10 @@ package com.soprasteria.initiatives.commons.api.clients;
  */
 final class ServicesUrls {
 
+    public static final String CONNECTOR_CORE_BANKING_A = "";
+    public static final String CONNECTOR_CORE_BANKING_A_TEST_URL = "";
+    public static final String CORE_BANKING_A_TEST_URL = "";
+    public static final String CORE_BANKING_A = "";
+    public static final String ACCOUNT_VIEWING_SERVICE = "";
+    public static final String ACCOUNT_VIEWING_SERVICE_TEST_URL = "";
 }
