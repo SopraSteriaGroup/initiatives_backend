ba99d04622531c7604c4e7c77fb02f0db5ff8f70
Charles-Éric Giraud <charleseric.giraud@gmail.com>
2017-04-16 22:04:27 +0200

    [user-service] change http method

--- user-service/src/main/java/com/soprasteria/initiatives/user/api/UserResource.java
+++ user-service/src/main/java/com/soprasteria/initiatives/user/api/UserResource.java
@@ -2,8 +2,7 @@ package com.soprasteria.initiatives.user.api;
 
 import com.soprasteria.initiatives.user.domain.Utilisateur;
 import com.soprasteria.initiatives.user.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,7 +22,7 @@ public class UserResource {
         this.userService = userService;
     }
 
-    @PutMapping(ApiConstants.SOUSCRIRE_URI)
+    @PostMapping(ApiConstants.SOUSCRIRE_URI)
     public void souscrire(Utilisateur utilisateur) {
         userService.souscrire(utilisateur);
     }
